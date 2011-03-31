c0e8afd90e96548dec8ec7e2c393c2d852cdcc28
james sa <james.sa.2013@gmail.com>
2011-03-31 11:13:39 -0400

    init windows app

---
+++ Vocabulous/VocabulousAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  VocabulousAppDelegate.h
+//  Vocabulous
+//
+//  Created by james sa on 2011/3/31.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VocabulousAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ Vocabulous/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Vocabulous
+//
+//  Created by james sa on 2011/3/31.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
