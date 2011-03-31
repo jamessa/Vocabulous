ee38a9fd0d09abcc295092ebebf3d7f8d41904c7
james sa <james.sa.2013@gmail.com>
2011-04-01 01:39:40 +0800

    section header

--- Vocabulous/WordListTableViewController.m
+++ Vocabulous/WordListTableViewController.m
@@ -138,6 +138,10 @@
     return cell;
 }
 
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return [sections objectAtIndex:section];
+}
 /*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
