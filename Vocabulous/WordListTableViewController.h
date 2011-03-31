196972a57d116c3be5469909eca2ce5d6d1873bb
james sa <james.sa.2013@gmail.com>
2011-03-31 23:19:12 +0800

    init UITableViewController

---
+++ Vocabulous/WordListTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  WordListTableViewController.h
+//  Vocabulous
+//
+//  Created by james sa on 2011/3/31.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface WordListTableViewController : UITableViewController {
+    
+}
+
+@end
